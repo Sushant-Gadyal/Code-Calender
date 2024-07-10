b77077b886a797ae82d5acece38a1e948fae4e73
Sushant-Gadyal <sushantgadyal19@gmail.com>
2024-07-10 13:03:38 +0530

    Logout Logic  done

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React , {useState, useEffect}from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 function Navbar() {
 
   const [isDashboard, setisDashboard] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (localStorage.getItem("useremail")) {
@@ -11,6 +13,12 @@ function Navbar() {
     }
     console.log("aiti");
   }, []);
+
+  const handlelogout = ()=>{
+    localStorage.removeItem("useremail");
+    navigate("/")
+
+  }
 
   return (
     <div>
@@ -25,9 +33,10 @@ function Navbar() {
         <div>
           {isDashboard===true ? 
             (
-              <Link to="/logout">
+              <Link to="/">
                 <button
                   type="button"
+                  onClick = {handlelogout}
                   className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                   >
                   Logout
