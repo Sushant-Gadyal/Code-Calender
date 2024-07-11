897a507400fcd1df05a65f807c240b795aaf880e
Sushant-Gadyal <sushantgadyal19@gmail.com>
2024-07-11 20:20:43 +0530

    Updated Schema

--- BackEnd/model/UserModel.js
+++ BackEnd/model/UserModel.js
@@ -1,36 +1,46 @@
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-    username : {
-        type : String,
-        required : true,
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  lc_handle: {
+    type: String,
+  },
+  cf_handle: {
+    type: String,
+  },
+  submission_date: {
+    type: Array,
+    default: [],
+  },
+  last_lc_updated: {
+    date: {
+      type: String,
     },
-    email:{
-        type : String,
-        required : true,
+    count: {
+      type: String,
     },
-    password : {
-        type : String,
-        required : true,
+  },
+  last_cf_updated: {
+    date: {
+      type: String,
     },
-    lc_handle : {
-        type : String,
+    count: {
+      type: String,
     },
-    cf_handle : {
-        type : String,
-    },
-    submission_date : {
-        type : Array,
-        default : []
-    },
-    last_lc_updated:{
-        type : Date,
-    },
-    last_cf_updated  : {
-        type : Date,
-    }
-})
+  },
+});
 
-const UserModel = mongoose.model("UserModel",UserSchema);
+const UserModel = mongoose.model("UserModel", UserSchema);
 
 module.exports = UserModel;
